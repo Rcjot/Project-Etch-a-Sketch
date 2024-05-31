7bd3c5df08ee991043c0f84c6eb30ae1fa204b59
Rcjot <rcappy495@gmail.com>
2024-05-31 16:31:55 +0800

    rainbows and buttons!

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Project-Etch-a-Sketch
+
+Hi! 
+
+This is my Etch a Sketch!
+
+
+first off i didnt know how to make a grid using flexbox, so i had to look it up..
+
+had to do math so boxes are contained in the container!
+
+ASFKJDPOFIJSK... i didnt know how removing elements work.. i learned how to make references to elements made in a function....
+
+rainbows and buttons work now! 16:30;
+
+i dont know how to make it so that box wouldn't be changed from rainbow color to black vicev esrsa
--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const container = document.querySelector("#container");
 const canvaButton = document.querySelector('#canvasButton');
 const rainbow = document.querySelector('#rainbow');
+const black = document.querySelector('#black');
+black.setAttribute('style', 'background-color: rgba(60,41,55, 0.5)');
 const numRange = document.querySelector('#numRange');
 const numInput = document.querySelector('#numInput');
-numInput.value = 5;
+numInput.value = 16;
 let rowContainerMain; // do this
 let size = numInput.value;
 let first = true;
+let rainbowBool = false;
 //  let size = 16;
 
 
@@ -23,37 +26,59 @@ function createCanvas(mySize){
         gridRow.classList.add("gridRow");
         rowContainer.appendChild(gridRow);
         for (let j = 1; j <=mySize; j++){
+            const rainbowColor = makeRainbow(); // put it here, 
+            const blackColor = ';background-color: black';
             let opacity = 0;
             const gridBox = document.createElement("div");
             gridBox.classList.add("gridColumn");
             gridBox.setAttribute('style', myAttribute);
             gridRow.appendChild(gridBox);
             gridBox.addEventListener("mouseover", () => {
+                const myFinalColor = (rainbowBool === true) ? rainbowColor : blackColor;
+                console.log(myFinalColor);
+                const myFinalAttribute = myAttribute + myFinalColor +'; border:0px';
                 opacity += 0.1;
-                const myFinalAttribute = myAttribute + '; background-color: black; border:0px';
-                console.log(myAttribute);
-                gridBox.setAttribute('style', myFinalAttribute + ' ;opacity: ' +opacity);
+                gridBox.setAttribute('style', myFinalAttribute +';opacity:'+opacity);
+                
             })
         }    
     }
 
 }
 
+function makeRainbow(){
+    let redV = Math.floor(Math.random()*100);
+    let greenV = Math.floor(Math.random()*100);
+    let blueV = Math.floor(Math.random()*100);
+    let opacV = Math.floor(Math.random()*10)/10;
+    let bgcolor = ';background-color: rgba('+ redV + ','+ greenV + ','+ blueV +')';
+    return bgcolor;
 
 
-
-
-
+}
 createCanvas(size);
 
+
+
+
+
 rainbow.addEventListener("click", ()=>{
-    removeCanvas();
+    rainbowBool = true;
+    rainbow.setAttribute('style', 'background-color: rgba(60,41,55, 0.5)');
+    black.removeAttribute('style');
+});
+black.addEventListener("click", () =>{
+    rainbowBool = false;
+    black.setAttribute('style', 'background-color: rgba(60,41,55, 0.5)');
+    rainbow.removeAttribute('style');
+
 })
+
 
 canvaButton.addEventListener("click", () => {
     first = false;
     size = numInput.value;    
     console.log(size);  
-    rowContainerMain.remove();
+    rowContainerMain.remove(); // the remove here
     createCanvas(size);
 })
