4be82ca4fd5c4c2a891fbb9cba9b621cc8c6a58f
Rcjot <rcappy495@gmail.com>
2024-05-31 12:25:37 +0800

    able to finish when hover draw!

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 const rowContainer = document.querySelector("#row-container");
 
-let size = 9;
+let size = 16;
 let sizepx = 700/size;
-let sizeString = 'height: '+sizepx+'px; width:'+sizepx +'px';
+let myAttribute = 'height: '+sizepx+'px; width:'+sizepx +'px';
 for (let i = 1; i <= size; i++) {
     const gridRow = document.createElement("div");
     gridRow.classList.add("gridRow");
     rowContainer.appendChild(gridRow);
     for (let j = 1; j <=size; j++){
+        let opacity = 0;
         const gridBox = document.createElement("div");
         gridBox.classList.add("gridColumn");
-        // gridBox.textContent = "HALO" + i;
-        gridBox.setAttribute('style', sizeString);
+        gridBox.setAttribute('style', myAttribute);
         gridRow.appendChild(gridBox);
-    }
-
-    
-    
-    
+        gridBox.addEventListener("mouseover", () => {
+            opacity += 0.1;
+            const myFinalAttribute = myAttribute + '; background-color: black; border:0px';
+            console.log(myAttribute);
+            gridBox.setAttribute('style', myFinalAttribute + ' ;opacity: ' +opacity);
+        })
+    }    
 }
+
+
+
